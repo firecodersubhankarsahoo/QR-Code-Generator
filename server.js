8f317cd4057847f2d37724960fd820a6403e83a0
subhankar sau <115922995+firecodersubhankarsahoo@users.noreply.github.com>
2025-01-17 23:47:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
+
+
+
+
 const express = require('express');
 const qr = require('qr-image');
 const app = express();
 
+// Serve static files from the 'public' folder
 app.use(express.static('public'));
 
+// Define the QR code generation endpoint
 app.get('/generate', (req, res) => {
     const link = req.query.link;
-    const qrCode = qr.image(link, { type: 'png', size: 10, color: '#0000ff' });
+    if (!link) {
+        return res.status(400).send('Invalid link');
+    }
+    const qrCode = qr.image(link, { type: 'png', size: 10 });
     res.type('png');
     qrCode.pipe(res);
 });
 
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
